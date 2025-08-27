baf8e5dd6ec77d0bf65296aae54cbd7ef56a1334
kelvyn2012 <kudufo@gmail.com>
2025-08-27 20:25:28 +0100

    created my routing and also my views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Message
 
-# Create your views here.
+
+def home(request):
+    rooms = Room.objects.all()
+    return render(request, "chat/home.html", {"rooms": rooms})
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "chat/register.html", {"form": form})
+
+
+@login_required
+def room(request, room_name):
+    room = Room.objects.get(slug=room_name)
+    messages = Message.objects.filter(room=room).order_by("date_added")[:50]
+    return render(
+        request,
+        "chat/room.html",
+        {"room": room, "messages": messages, "user": request.user},
+    )
